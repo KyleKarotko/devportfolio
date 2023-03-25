5a19569beebec279afb6def9ebba404028543c9a
kyle_karotko <kkrtk7@duck.com>
2023-03-25 14:51:48 -0400

    adding required material to the about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import img from '../assets/images/Avatar1.png'
+
+
 function About() {
   return (
     <div>
-   
+        <img src={img} />
+
+
+      <p>
+        I am a career changer with a passion for technology and problem-solving,
+        currently enrolled in a coding bootcamp to transition into a career as a
+        Full Stack Developer. My previous roles as a Sales Manager and Personal
+        Trainer have allowed me to develop a strong foundation in leadership,
+        project management, and customer service. Through my experiences, I have
+        learned the importance of adapting to new challenges and finding
+        innovative solutions to complex problems.
+      </p>
+      <p>
+        In addition to my work experience, I have also completed courses in web
+        development, programming, and software engineering. I am eager to apply
+        my newly acquired skills and knowledge to a career in software
+        development. My dedication to self-improvement and commitment to
+        learning will make me a valuable asset to any team.
+      </p>
     </div>
   );
 }
