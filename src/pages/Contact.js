97c924ea8e2db2f5bfd5b8ae5f43b58971c5224e
kyle_karotko <kkrtk7@duck.com>
2023-03-25 22:53:58 -0400

    adding contact form fields/ validation/ logic

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,51 @@
-import React from "react";
-function Contact (){
-    return (
-        <div>
-            
-        </div>
-    )
+import React, { useState } from 'react';
+
+function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email, message);
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Message:
+        <textarea
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+
 export default Contact
 
 // WHEN I am presented with the Contact section
