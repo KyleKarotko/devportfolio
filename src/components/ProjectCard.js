30811cc83fa8d854ec8b6a1621f6d794e7c659ab
kyle_karotko <kkrtk7@duck.com>
2023-03-26 22:19:20 -0400

    adding styling to porfolio section/ adding links to all projects as well as props

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function ProjectCard({ title, desc, img }) {
+function ProjectCard({ title, desc, img, depLink, gitLink }) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={img} alt={`${title} screenshot`} />
-      <Card.Body>
+      <Card.Body style={{ textAlign: 'center' }}>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{desc}</Card.Text>
-        <Button variant="primary">Deployed Application</Button>
-        <Button variant="primary">GitHub Repository</Button>
+        <Button variant="primary" href={depLink} target="_blank" rel="noopener noreferrer">
+          Deployed Application
+        </Button>{' '}
+        <Button variant="primary" href={gitLink} target="_blank" rel="noopener noreferrer">
+          GitHub Repository
+        </Button>{' '}
       </Card.Body>
     </Card>
   );
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,63 +6,81 @@ import project3 from '../assets/images/JSQ3.png';
 import project4 from '../assets/images/NT1.png';
 import project5 from '../assets/images/TB1.png';
 import project6 from '../assets/images/JATE2.png';
+import '../styles/Portfolio.css';
+
 function Portfolio() {
-  const projects = [
-    {
-      id: 1,
-      title: 'First Portfolio',
-      desc: 'When a potential employer wants to sample my previous work they load my portfolio. When they do, they are presented with my name, a recent photo, and links to sections about me, my work, and how to contact me.',
-      img: project1,
-    },
-    {
-      id: 2,
-      title: 'Movie Time',
-      desc: 'Allow users to choose a movie to watch based on information about a movie and displaying its trailer. Achieving this functionality comes from the use of JavaScript, more specifically the server side API',
-      img: project2,
-    },
-    {
-      id: 3,
-      title: 'JavaScript Quiz',
-      desc: 'JavaScript quiz app where the user takes a quiz, with a timer that starts when they click the start button, and are presented with a series of questions. If a question is answered incorrectly, time is deducted from the clock. When all questions are answered or the timer reaches zero, the game is over, and the user can save their initials and their score.',
-      img: project3,
-    },
-    {
-      id: 4,
-      title: 'Note Taker',
-      desc: 'An application that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.',
-      img: project4,
-    },
-    {
-      id: 5,
-      title: 'Travel Forum',
-      desc: 'This application is used to enable travelers and vacation seekers to blog about locations they have visited. This application will also serve fellow travelers in learning about new locations they may want to visit.',
-      img: project5,
-    },
-    {
-      id: 6,
-      title: 'Progressive Web App - Text Editor',
-      desc: 'This is an installable text editor web application that contains a client server folder structure. It uses webpack to bundle JS files. It is able to function with past and present generations of JS without error. The editor uses Indexed DB for storage on the client for any data entered in. This data will persist without an internet connection.',
-      img: project6,
-    },
-  ];
+    const projects = [
+        {
+          id: 1,
+          title: 'First Portfolio',
+          desc: 'My portfolio showcases my work and provides a snapshot of who I am as a developer. It features information about me, my work, and ways to get in touch.',
+          img: project1,
+          depLink: 'https://kylekarotko.github.io/kkportfolio/',
+          gitLink: 'https://github.com/KyleKarotko/kkportfolio',
+        },
+        {
+          id: 2,
+          title: 'Movie Time',
+          desc: 'Movie Time is a web app that allows users to browse and watch movies. Powered by server-side APIs, it presents movie information and trailers to users with a simple interface.',
+          img: project2,
+          depLink: 'https://naidiri1.github.io/Movie-Time/',
+          gitLink: 'https://github.com/Naidiri1/Movie-Time',
+
+        },
+        {
+          id: 3,
+          title: 'JavaScript Quiz',
+          desc: 'The JavaScript quiz app tests the user\'s knowledge of JavaScript. It features a timer that starts when the quiz begins and deducts time for incorrect answers. Users can save their initials and their score when the quiz ends.',
+          img: project3,
+          depLink: 'https://kylekarotko.github.io/JSQ/',
+          gitLink: 'https://github.com/KyleKarotko/JSQ',
+
+        },
+        {
+          id: 4,
+          title: 'Note Taker',
+          desc: 'The Note Taker app is a useful tool for taking and saving notes. Built with Express.js, it stores note data in a JSON file and retrieves it when needed.',
+          img: project4,
+          depLink: 'https://fast-basin-86731.herokuapp.com/',
+          gitLink: 'https://github.com/KyleKarotko/Note_Taker/',
+
+        },
+        {
+          id: 5,
+          title: 'Travel Forum',
+          desc: 'The Travel Forum is a platform for travelers to share their experiences and recommendations with other travelers. It is designed to help users discover new travel destinations and make informed decisions about their trips.',
+          img: project5,
+          depLink: 'https://travel-forum-p2.herokuapp.com/',
+          gitLink: 'https://github.com/trudman/travel-forum'
+        },
+        {
+          id: 6,
+          title: 'Progressive Web App - Text Editor',
+          desc: 'The Text Editor is a progressive web app that can be installed on any device. It features a client-server folder structure and uses webpack to bundle JS files. The editor uses Indexed DB for storage on the client and can function offline without losing any data.',
+          img: project6,
+          depLink: 'https://desolate-thicket-86948.herokuapp.com/',
+          gitLink: 'https://github.com/KyleKarotko/pwa-textEditor/',
+
+        },
+      ];
+      
   return (
-    <div>
-      {projects.map((project) => {
-        return (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            desc={project.desc}
-            img={project.img}
-          />
-        );
-      })}
+    <div className="container">
+      <div className="row justify-content-center">
+        {projects.map((project) => (
+          <div className="col-lg-4 col-md-6 mb-5" key={project.id}>
+            <ProjectCard
+              title={project.title}
+              desc={project.desc}
+              img={project.img}
+              depLink={project.depLink}
+              gitLink={project.gitLink}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Portfolio;
-
-// WHEN I am presented with the Portfolio section
-// THEN I see titled images of six of the developer’s applications with links to
-// both the deployed applications and the corresponding GitHub repositories
