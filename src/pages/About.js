ec1475687c5a9be3d8451e883eb840a23f956129
kyle_karotko <kkrtk7@duck.com>
2023-03-27 12:07:42 -0400

    changing bios in home and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,26 +6,33 @@ function About() {
   return (
     <div className="about-container">
       <div className="about-avatar">
-        <img src={img} alt="Avatar of a bearded smiling bald guy wearing glasses and a purple hoody against a green checkard backgrond" />
+        <img
+          src={img}
+          alt="Avatar of a bearded smiling bald guy wearing glasses and a purple hoody against a green checkard backgrond"
+        />
       </div>
       <div className="about-text">
         <div className="card about-card">
           <div className="card-body">
             <p>
-              I am a career changer with a passion for technology and problem-solving,
-              currently enrolled in a coding bootcamp to transition into a career as a
-              Full Stack Developer. My previous roles as a Sales Manager and Personal
-              Trainer have allowed me to develop a strong foundation in leadership,
-              project management, and customer service. Through my experiences, I have
-              learned the importance of adapting to new challenges and finding
-              innovative solutions to complex problems.
+              Hello, my name is Kyle and I am excited to introduce myself as a
+              passionate and dedicated individual with a diverse background in
+              sales management, personal training, and now, full stack web
+              development. I am currently enrolled in a full stack web
+              development bootcamp, where I am gaining hands-on experience and
+              building a strong foundation in programming languages such as
+              JavaScript, HTML, CSS, and various frameworks such as React,
+              Node.js, and Express.
             </p>
             <p>
-              In addition to my work experience, I have also completed courses in web
-              development, programming, and software engineering. I am eager to apply
-              my newly acquired skills and knowledge to a career in software
-              development. My dedication to self-improvement and commitment to
-              learning will make me a valuable asset to any team.
+              Throughout my professional career, I have honed valuable skills in
+              leadership, project management, and customer service that have
+              been transferable to my current journey in web development. I am
+              constantly seeking new challenges and opportunities to grow both
+              personally and professionally. I am eager to leverage my
+              experience and newfound skills to contribute to an innovative team
+              and be a part of shaping the future of technology. Thank you for
+              considering my profile.
             </p>
           </div>
         </div>
@@ -35,7 +42,6 @@ function About() {
 }
 
 export default About;
-
 
 // WHEN I am presented with the About Me section
 // THEN I see a recent photo or avatar of the developer and a short bio about them
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,48 @@ const Home = () => {
     <>
       <div className="card">
         <h1 className="heading">Hi, I'm Kyle.</h1>
-        <p className="subheading">I'm a web developer, and this is my portfolio. I specialize in full stack development using the MERN stack, and I have just completed the bootcamp at The Ohio State University. I'm passionate about learning, and I love using my skills to innovate and problem solve todays technology challenges.</p>
+        <p className="subheading">
+          I am a career changer with a passion for technology and
+          problem-solving, currently enrolled in a coding bootcamp to transition
+          into a career as a Full Stack Developer. My previous roles as a Sales
+          Manager and Personal Trainer have allowed me to develop a strong
+          foundation in leadership, project management, and customer service.
+          Through my experiences, I have learned the importance of adapting to
+          new challenges and finding innovative solutions to complex problems.
+        </p>
       </div>
 
       <div className="card">
         <h1 className="heading">Featured Work</h1>
         <ul className="project-list">
           <li className="project-li">
-            <a className="project-link" href='https://desolate-thicket-86948.herokuapp.com/'>Text Editor</a> - A progressive web app that can be installed on any device.
+            <a
+              className="project-link"
+              href="https://desolate-thicket-86948.herokuapp.com/"
+            >
+              Text Editor
+            </a>{' '}
+            - A progressive web app that can be installed on any device.
           </li>
           <li className="project-li">
-            <a className="project-link" href='https://travel-forum-p2.herokuapp.com/'>Travel Blog</a> - A platform for travelers to share their experiences and recommendations with other travelers.
+            <a
+              className="project-link"
+              href="https://travel-forum-p2.herokuapp.com/"
+            >
+              Travel Blog
+            </a>{' '}
+            - A platform for travelers to share their experiences and
+            recommendations with other travelers.
           </li>
           <li className="project-li">
-            <a className="project-link" href='https://naidiri1.github.io/Movie-Time/'>Movie Time</a> - A web app powered by server-side APIs, it presents movie information and trailers to users with a simple interface.
+            <a
+              className="project-link"
+              href="https://naidiri1.github.io/Movie-Time/"
+            >
+              Movie Time
+            </a>{' '}
+            - A web app powered by server-side APIs, it presents movie
+            information and trailers to users with a simple interface.
           </li>
         </ul>
       </div>
@@ -27,13 +55,23 @@ const Home = () => {
       <div className="card">
         <h1 className="heading">Get in Touch</h1>
         <ul className="contact-list">
-          <li className="contact-li"><a className="contact-link" href="mailto:karotko.dev@gmail.com">Email</a></li>
-          <li className="contact-li"><a className="contact-link" href="https://www.linkedin.com/in/kkarotko">LinkedIn</a></li>
+          <li className="contact-li">
+            <a className="contact-link" href="mailto:karotko.dev@gmail.com">
+              Email
+            </a>
+          </li>
+          <li className="contact-li">
+            <a
+              className="contact-link"
+              href="https://www.linkedin.com/in/kkarotko"
+            >
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </div>
     </>
   );
-}
+};
 
 export default Home;
-
